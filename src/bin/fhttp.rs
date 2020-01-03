84464febb66f69c339d6f4100fd62fbfad1d8e64
René Perschon <rene.perschon@akelius.de>
2020-01-03 21:34:31 +0100

    first impl of an executable

--- src/bin/fhttp.rs
+++ src/bin/fhttp.rs
@@ -1,3 +1,56 @@
+use clap::{App, Arg, crate_version, crate_authors};
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::process;
+use fhttp::Request;
+
 fn main() {
-    println!("fhttp");
+    let matches = App::new("fhttp")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("file-based http client")
+        .arg(Arg::with_name("files")
+            .multiple(true)
+            .required(true)
+            .min_values(1)
+            .value_name("FILES")
+            .help("the request files to execute"))
+        .get_matches();
+
+    let files = matches.values_of("files").unwrap()
+        .map(|file| PathBuf::from_str(file).unwrap())
+        .collect::<Vec<_>>();
+
+    let non_existent = files.iter()
+        .filter(|it| !it.exists())
+        .collect::<Vec<_>>();
+
+    if !non_existent.is_empty() {
+        for file in non_existent {
+            eprintln!("'{}' does not exist", file.to_str().unwrap())
+        }
+        process::exit(1);
+    }
+
+    let non_file = files.iter()
+        .filter(|it| !it.is_file())
+        .collect::<Vec<_>>();
+
+    if !non_file.is_empty() {
+        for file in non_file {
+            eprintln!("'{}' is not a file", file.to_str().unwrap())
+        }
+        process::exit(1);
+    }
+
+    let requests = files.into_iter()
+        .map(|it| {
+            let content = std::fs::read_to_string(&it).unwrap();
+            Request::parse(content, &it)
+        })
+        .collect::<Vec<_>>();
+
+    for req in requests {
+        println!("{:#?}", req);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ extern crate lazy_static;
 extern crate indoc;
 extern crate jsonpath_lib as jsonpath;
 extern crate serde_json;
+extern crate clap;
 
 mod request;
 mod response_handler;
