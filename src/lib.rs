8e79e12846e4d5a10b9524e2f6dec4e27da8622c
René Perschon <rene.perschon@akelius.de>
2020-01-03 20:55:40 +0100

    naive jsonpath response handler impl

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate reqwest;
 #[macro_use]
 extern crate lazy_static;
 extern crate indoc;
+extern crate jsonpath_lib as jsonpath;
+extern crate serde_json;
 
 mod request;
 mod response_handler;
--- src/response_handler.rs
+++ src/response_handler.rs
@@ -23,14 +23,64 @@ impl ResponseHandler for JsonPathResponseHandler {
         &self,
         body: &str
     ) -> String {
-        unimplemented!()
+        use jsonpath::Selector;
+        use serde_json::Value;
+
+        let value: Value = serde_json::from_str(body).unwrap();
+
+        let mut selector = Selector::new();
+        let mut json_path_results = selector
+            .str_path(&self.json_path).unwrap()
+            .value(&value)
+            .select()
+            .unwrap();
+        let result = match json_path_results.len() {
+            0 => Value::String("".into()),
+            _ => json_path_results[0].clone(),
+        };
+
+        match result {
+            Value::String(string) => string,
+            _ => serde_json::to_string(&result).unwrap()
+        }
     }
 }
 
-//data class JsonPathResponseHandler(
-//    private val jsonPath: String
-//) : ResponseHandler {
-//    override fun processBody(body: String): String {
-//        return JsonPath.read<Any>(body, jsonPath).toString()
-//    }
-//}
+#[cfg(test)]
+mod json_tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn should_apply_the_jsonpath_expression() {
+        let body = indoc!("
+            {
+                \"a\": {
+                    \"b\": {
+                        \"c\": \"success\"
+                    },
+                    \"c\": \"failure\"
+                }
+            }
+        ");
+        let result = JsonPathResponseHandler::new("$.a.b.c").process_body(body);
+
+        assert_eq!(result, "success");
+    }
+
+    #[test]
+    fn should_convert_numbers_to_string() {
+        let body = indoc!("
+            {
+                \"a\": {
+                    \"b\": {
+                        \"c\": 3.141
+                    }
+                }
+            }
+        ");
+        let result = JsonPathResponseHandler::new("$.a.b.c").process_body(body);
+
+        assert_eq!(result, "3.141");
+    }
+}
