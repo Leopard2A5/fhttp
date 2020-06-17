5ce469f14a86959d087359a199e9ba4524b3a2b0
René Perschon <rene.perschon@akelius.de>
2020-06-17 11:44:03 +0200

    remove unnecessary extern crate declarations in fhttp crate

--- fhttp/src/lib.rs
+++ fhttp/src/lib.rs
@@ -1,15 +1,1 @@
-extern crate reqwest;
-#[allow(unused)]
-#[macro_use]
-extern crate lazy_static;
-extern crate jsonpath_lib as jsonpath;
-extern crate serde;
-extern crate serde_json;
 extern crate clap;
-extern crate promptly;
-extern crate rand;
-extern crate uuid;
-#[cfg(test)]
-extern crate indoc;
-#[cfg(test)]
-extern crate maplit;
