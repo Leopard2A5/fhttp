f9bb06b92cbd0c2361f922c824214393e2f86772
René Perschon <rene.perschon@quantilope.com>
2023-01-01 21:39:23 +0100

    refactor curl.rs test to set up its own files

--- fhttp/tests/curl.rs
+++ fhttp/tests/curl.rs
@@ -2,25 +2,27 @@ extern crate assert_cmd;
 extern crate mockito;
 extern crate reqwest;
 
-use std::env;
-
+use anyhow::Result;
 use assert_cmd::Command;
 use async_std::task::block_on;
+use fhttp_core::execution::curl::Curl;
+use fhttp_core::path_utils::CanonicalizedPathBuf;
+use fhttp_core::request::body::Body;
+use fhttp_core::request::Request;
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::Method;
+use temp_dir::TempDir;
+use std::env;
+use wiremock::matchers::{body_string_contains, method, path};
 use wiremock::{Mock, MockServer, ResponseTemplate};
-use wiremock::matchers::{method, path, body_string_contains};
-
-use fhttp_core::execution::curl::Curl;
-use fhttp_core::request::Request;
-use fhttp_core::request::body::Body;
+use indoc::{indoc, formatdoc};
 
 #[test]
-fn curl_invocation_with_dependency() {
+fn curl_invocation_with_dependency() -> Result<()> {
     block_on(test())
 }
 
-async fn test() {
+async fn test() -> Result<()> {
     let mock_server = MockServer::start().await;
     env::set_var("URL", mock_server.uri());
     env::set_var("USERNAME", "username_secret");
@@ -29,8 +31,7 @@ async fn test() {
     Mock::given(method("POST"))
         .and(path("/token"))
         .and(body_string_contains("username_secret"))
-        .respond_with(ResponseTemplate::new(200)
-            .set_body_string(r#"{ "token": "jwt" }"#))
+        .respond_with(ResponseTemplate::new(200).set_body_string(r#"{ "token": "jwt" }"#))
         .expect(1)
         .mount(&mock_server)
         .await;
@@ -38,32 +39,71 @@ async fn test() {
     let mut headers = HeaderMap::new();
     headers.insert(
         HeaderName::from_static("authorization"),
-        HeaderValue::from_static("Bearer jwt")
+        HeaderValue::from_static("Bearer jwt"),
     );
     headers.insert(
         HeaderName::from_static("content-type"),
-        HeaderValue::from_static("application/json")
+        HeaderValue::from_static("application/json"),
     );
-    let req = Request {
+    let body = "{\n  \"name\": \"resource\"\n}".to_string();
+    let expectation = Request {
         method: Method::POST,
         url: format!("{}/resources", mock_server.uri()),
         headers,
-        body: Body::Plain("{\n  \"name\": \"resource\"\n}".to_string()) ,
-        response_handler: None
-    };
-    let req = req.curl();
+        body: Body::Plain(body.clone()),
+        response_handler: None,
+    }.curl();
 
-    Command::cargo_bin("fhttp").unwrap()
+    let workdir = TempDir::new()?;
+
+    let token = workdir.child("token.http");
+    std::fs::write(
+        &token,
+        indoc!("
+            POST ${env(URL)}/token
+            Content-Type: application/json
+            
+            {
+                \"username\": \"${env(USERNAME)}\",
+                \"password\": \"${env(PASSWORD)}\"
+            }
+            
+            > {%
+                json $.token
+            %}
+        ").as_bytes()
+    )?;
+
+    let req = workdir.child("req.http");
+    std::fs::write(
+        &req,
+        formatdoc!("
+            POST ${{env(URL)}}/resources
+            Authorization: Bearer ${{request(\"token.http\")}}
+            Content-Type: application/json
+
+            {body}
+
+            > {{%
+                json $.id
+            %}}
+        ", body = body).as_bytes()
+    )?;
+
+    let workdir = CanonicalizedPathBuf::new(workdir.path());
+    let req = workdir.join(req);
+
+    Command::cargo_bin("fhttp")
+        .unwrap()
         .arg("--curl")
-        .arg("../resources/it/curl/create.http")
+        .arg(req.to_str())
         .assert()
         .success()
         .stderr(format!(
             "POST {uri}/token... 200 OK\nPOST {uri}/resources... ",
-            uri=mock_server.uri(),
+            uri = mock_server.uri(),
         ))
-        .stdout(format!(
-            "\n{curl}\n",
-            curl=req,
-        ));
+        .stdout(format!("\n{curl}\n", curl = expectation));
+
+    Ok(())
 }
