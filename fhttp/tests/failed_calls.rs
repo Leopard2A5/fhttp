1ae5205ace1056d77cab0ada3f7c78a6d6756ceb
René Perschon <rene.perschon@quantilope.com>
2023-01-04 14:42:15 +0100

    refactor failed_calls.rs test to set up its own files

--- fhttp/tests/failed_calls.rs
+++ fhttp/tests/failed_calls.rs
@@ -1,15 +1,39 @@
 extern crate assert_cmd;
 extern crate mockito;
-
-use std::env;
+extern crate rstest;
 
 use assert_cmd::Command;
+use fhttp_core::path_utils::CanonicalizedPathBuf;
 use mockito::mock;
+use rstest::{rstest, fixture};
+use temp_dir::TempDir;
 
-#[test]
-fn should_stop_execution_on_status_400() {
-    let url = &mockito::server_url();
-    env::set_var("URL", &url);
+struct TestData {
+    pub _workdir: TempDir,
+    pub req1: CanonicalizedPathBuf,
+    pub req2: CanonicalizedPathBuf,
+}
+
+#[fixture]
+fn test_data() -> TestData {
+    let workdir = TempDir::new().unwrap();
+
+    let req1 = workdir.child("req1");
+    std::fs::write(&req1, "GET ${env(URL)}/1".as_bytes()).unwrap();
+
+    let req2 = workdir.child("req2");
+    std::fs::write(&req2, "GET ${env(URL)}/2".as_bytes()).unwrap();
+
+    TestData {
+        _workdir: workdir,
+        req1: CanonicalizedPathBuf::new(req1),
+        req2: CanonicalizedPathBuf::new(req2),
+    }
+}
+
+#[rstest]
+fn should_stop_execution_on_status_400(test_data: TestData) {
+    let url = mockito::server_url();
 
     let one = mock("GET", "/1")
         .with_status(400)
@@ -21,29 +45,30 @@ fn should_stop_execution_on_status_400() {
         .create();
 
     let assert = Command::cargo_bin("fhttp").unwrap()
-        .arg("../resources/it/requests/1.http")
-        .arg("../resources/it/requests/2.http")
+        .env("URL", &url)
+        .arg(test_data.req1.to_str())
+        .arg(test_data.req2.to_str())
         .assert();
 
     assert
         .failure()
         .stderr(format!(
             "GET {base}/1... 400 Bad Request\nError: invalid param\n",
-            base=url
+            base=&url
         ));
 
     one.assert();
     two.assert();
 }
 
-#[test]
-fn should_stop_execution_on_connection_issues() {
+#[rstest]
+fn should_stop_execution_on_connection_issues(test_data: TestData) {
     let url = "http://unreachableurl.foobar";
-    env::set_var("URL", url);
 
     let mut cmd = Command::cargo_bin("fhttp").unwrap();
-    cmd.arg("../resources/it/requests/1.http");
-    cmd.arg("../resources/it/requests/2.http");
+    cmd.env("URL", &url);
+    cmd.arg(test_data.req1.to_str());
+    cmd.arg(test_data.req2.to_str());
 
     let output = cmd.output().unwrap();
     let stderr = String::from_utf8(output.stderr).unwrap();
@@ -52,8 +77,9 @@ fn should_stop_execution_on_connection_issues() {
     assert.failure();
 
     let expectation = format!(
-        "GET {base}/1... Error: error sending request for url (http://unreachableurl.foobar/1): error trying to connect:",
-        base=url
+        "GET {base}/1... Error: error sending request for url ({url}/1): error trying to connect:",
+        url=&url,
+        base=url,
     );
 
     assert!(stderr.contains(&expectation));
