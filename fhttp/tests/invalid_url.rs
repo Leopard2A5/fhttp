35a19d224d9ad6875a2b33ced2488c6b7d60ed2d
René Perschon <rene.perschon@quantilope.com>
2023-03-07 16:28:43 +0100

    refactor invalid_url.rs

--- fhttp/tests/invalid_url.rs
+++ fhttp/tests/invalid_url.rs
@@ -1,11 +1,21 @@
 extern crate assert_cmd;
 
 use assert_cmd::Command;
+use fhttp_test_utils::write_test_file;
+use temp_dir::TempDir;
 
 #[test]
 fn should_handle_invalid_url() {
+    let workdir = TempDir::new().unwrap();
+
+    let invalid_url = write_test_file(
+        &workdir,
+        "invalid_url.http",
+        "DELETE notAValidUrl"
+    ).unwrap();
+
     let assert = Command::cargo_bin("fhttp").unwrap()
-        .arg("../resources/it/requests/invalid_url.http")
+        .arg(invalid_url.to_str())
         .assert();
 
     assert
