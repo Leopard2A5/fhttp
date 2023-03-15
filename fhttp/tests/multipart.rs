1d2061083e35226c2d307226cad0c096a3446457
René Perschon <rene.perschon@quantilope.com>
2023-03-15 21:25:05 +0100

    refactor multipart test

--- fhttp/tests/multipart.rs
+++ fhttp/tests/multipart.rs
@@ -2,21 +2,46 @@ extern crate assert_cmd;
 extern crate wiremock;
 extern crate async_std;
 
-use std::env;
-
 use assert_cmd::Command;
-use async_std::task::block_on;
+use fhttp_test_utils::write_test_file;
+use indoc::formatdoc;
+use rstest::rstest;
+use temp_dir::TempDir;
 use wiremock::{MockServer, Mock, ResponseTemplate};
 use wiremock::matchers::{method, path, body_string_contains};
 
-#[test]
-fn test() {
-    block_on(async_test());
-}
-
-async fn async_test() {
+#[rstest]
+async fn multipart_test() {
     let mock_server = MockServer::start().await;
-    env::set_var("URL", mock_server.uri());
+
+    let workdir = TempDir::new().unwrap();
+
+    let content1 = write_test_file(
+        &workdir,
+        "multipart_content_1.txt",
+        "this is a file\n"
+    ).unwrap();
+
+    let content2 = write_test_file(
+        &workdir,
+        "multipart_content_2.txt",
+        "this is another file\n"
+    ).unwrap();
+
+    let multipart = write_test_file(
+        &workdir,
+        "multipart.http",
+        &formatdoc!(
+            "
+                POST ${{env(URL)}}/
+
+                ${{file(\"file\", \"{file1}\")}}
+                ${{file(\"data\", \"{file2}\")}}
+            ",
+            file1 = content1.to_str(),
+            file2 = content2.to_str(),
+        )
+    ).unwrap();
 
     Mock::given(method("POST"))
         .and(path("/"))
@@ -31,7 +56,8 @@ async fn async_test() {
         .await;
 
     Command::cargo_bin("fhttp").unwrap()
-        .arg("../resources/it/requests/multipart.http")
+        .env("URL", mock_server.uri())
+        .arg(multipart.to_str())
         .assert()
         .success();
 }
