ce2b1bc8af86d5ae97ea82bf1f70d4188fbffd80
René Perschon <rene.perschon@quantilope.com>
2023-03-20 19:06:25 +0100

    refactor timeout.rs

--- fhttp/tests/timeout.rs
+++ fhttp/tests/timeout.rs
@@ -1,23 +1,27 @@
 extern crate assert_cmd;
-extern crate async_std;
 extern crate wiremock;
 
-use std::env;
 use std::time::Duration;
 
 use assert_cmd::Command;
-use async_std::task::block_on;
+use fhttp_test_utils::write_test_file;
+use indoc::formatdoc;
+use rstest::rstest;
+use temp_dir::TempDir;
 use wiremock::{Mock, MockServer, ResponseTemplate};
 use wiremock::matchers::method;
 
-#[test]
-fn should_apply_timeouts() {
-    block_on(async_test());
-}
-
-async fn async_test() {
+#[rstest]
+async fn should_apply_timeouts() {
+    let workdir = TempDir::new().unwrap();
     let mock_server = MockServer::start().await;
-    env::set_var("URL", mock_server.uri());
+    let url = mock_server.uri();
+
+    let request = write_test_file(
+        &workdir,
+        "request.http",
+        "GET ${env(URL)}/1\n"
+    ).unwrap();
 
     Mock::given(method("GET"))
         .respond_with(ResponseTemplate::new(200)
@@ -28,12 +32,17 @@ async fn async_test() {
         .await;
 
     Command::cargo_bin("fhttp").unwrap()
+        .env("URL", &url)
         .arg("--timeout-ms").arg("500")
-        .arg("../resources/it/requests/1.http")
+        .arg(request.to_str())
         .assert()
         .failure()
-        .stderr(format!(
-            "GET {uri}/1... Error: error sending request for url ({uri}/1): operation timed out\n\nCaused by:\n    operation timed out\n",
-            uri=mock_server.uri()
+        .stderr(formatdoc!(
+            "GET {uri}/1... Error: error sending request for url ({uri}/1): operation timed out
+
+            Caused by:
+                operation timed out
+            ",
+            uri = url
         ));
 }
